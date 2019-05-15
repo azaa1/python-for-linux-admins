4afe63f140bee86501a5bdc2d62ca6a601836175
azaa1 <azaa.chi11@gmail.com>
2019-05-15 06:23:10 -0500

    netstat.py modified

--- netstat.py
+++ netstat.py
@@ -7,7 +7,7 @@ def netstat_func():
     command = "netstat"
     command_arg = "-tulpn"
     print ("Gathering server port information with %s command:\n" % command)
-    subprocess.call(command_arg, command)
+    subprocess.call([command_arg, command])
 
 def main():
     netstat_func()
